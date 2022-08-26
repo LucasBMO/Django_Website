f7697f7827f2128468b9cc6b6a3c05ab650263c4
LucasBMO <lucas_15.tm@hotmail.com>
2022-08-26 16:14:31 -0300

    Initial commit

---
+++ mysite/register/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, redirect
+from .forms import RegisterForm
+from django.views.decorators.csrf import csrf_exempt
+
+
+# Create your views here.
+@csrf_exempt
+def register(response):
+    if response.method == "POST":
+        form = RegisterForm(response.POST)
+        if form.is_valid():
+            form.save()
+
+        return redirect("/home")
+    else:
+        form = RegisterForm()
+
+    return render(response, "register/register.html", {"form": form})
